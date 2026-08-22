91840e7de95362cee3dd4b8ac482ee104b228faa
Elton Silva <eltoncarsilva73@gmail.com>
2026-08-22 17:20:18 -0300

    created a simple window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
-
-using namespace std;
+#include <GLFW/glfw3.h>
 
 int main()
 {
-	cout << "Hello CMake ." << endl;
+
+	GLFWwindow* window;
+
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(640, 480, "game project", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }

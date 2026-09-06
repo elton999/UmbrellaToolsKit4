bdee4359a83267762cf73c9f7323e8008e4d5353
Elton Silva <eltoncarsilva73@gmail.com>
2026-09-06 19:31:49 -0300

    created umbrella lib

--- tests/src/engine/core/platform/FileSystemTest.cpp
+++ tests/src/engine/core/platform/FileSystemTest.cpp
@@ -2,16 +2,16 @@
 #include "../../../../../src/engine/core/platform/FileSystem.h"
 #include <cstdlib>
 
-TEST_CASE("check write and read string", "[write and read][sting == null]")
+TEST_CASE("check write and read string", "[write and read]")
 {
-	int randomNum = rand() % 101;
+	int randomNum = rand();
 
-	Umbrella::FileSystem::Write("filesystem.txt", std::to_string(randomNum));
-	Umbrella::FileSystem::Write("filesystem.txt", std::to_string(randomNum));
-	Umbrella::FileSystem::Write("filesystem.txt", std::to_string(randomNum));
+	Umbrella::FileSystem::Write("filesystem.txt", std::to_string(randomNum) + "teste1");
+	Umbrella::FileSystem::Write("filesystem.txt", std::to_string(randomNum) + "teste2");
+	Umbrella::FileSystem::Write("filesystem.txt", std::to_string(randomNum) + "teste3");
 
 	std::string result = Umbrella::FileSystem::Read("filesystem.txt");
 
-	REQUIRE(result == std::to_string(randomNum));
+	REQUIRE(result == std::to_string(randomNum)+"teste3");
 }
 

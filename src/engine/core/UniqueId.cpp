86755334bab29976e30f1814fc8d1260810e31ae
Elton Silva <eltoncarsilva73@gmail.com>
2026-09-13 02:05:41 -0300

    created logic to unique id

--- src/engine/core/UniqueId.cpp
+++ src/engine/core/UniqueId.cpp
@@ -2,5 +2,21 @@
 
 std::string Umbrella::UniqueId::GetId()
 {
-    return nullptr;
+    GUID guid;
+    HRESULT hCreateGuid = CoCreateGuid(&guid);
+
+    if (FAILED(hCreateGuid))
+        return "";
+
+    wchar_t wStrBuffer[40];
+
+    int result = StringFromGUID2(guid, wStrBuffer, 40);
+
+    if (result == 0)
+        return "";
+
+    std::wstring wstr(wStrBuffer);
+    std::string str(wstr.begin(), wstr.end());
+
+    return str;
 }
--- src/engine/core/UniqueId.h
+++ src/engine/core/UniqueId.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include "combaseapi.h"
 
 namespace Umbrella
 {

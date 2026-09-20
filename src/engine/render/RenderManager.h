14a692ec39cfd93e43c1f822fd522390c565a450
Elton Silva <eltoncarsilva73@gmail.com>
2026-09-19 22:14:41 -0300

    created methods of layers and components

--- src/engine/render/RenderManager.h
+++ src/engine/render/RenderManager.h
@@ -1,11 +1,16 @@
 #pragma once
 
 #include "../UmbrellaRoot.h"
+#include "IRenderComponent.h"
+#include <string>
+#include <map>
 
 namespace Umbrella
 {
     class RenderManager
     {
+        private:
+            std::list<std::string, Umbrella::IRenderComponent*> _layersAndComponents;
 
         public:
             RenderManager(UmbrellaRoot* root);
@@ -13,5 +18,14 @@ namespace Umbrella
             void StartUp();
             void Run();
             void ShutDown();
+
+            void AddRenderComponent(IRenderComponent* renderComponent);
+            void AddRenderComponent(IRenderComponent *renderComponent, std::string layer);
+            void RemoveRenderComponet(IRenderComponent* renderComponent);
+            bool HasComponent(IRenderComponent* renderComponent);
+
+            std::string GetComponentLayer(IRenderComponent* renderComponent);
+            void AddLayer(std::string layer);
+            bool HasLayer(std::string layer);
     };
 }
